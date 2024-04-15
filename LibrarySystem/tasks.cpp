ac0e28027681bdf02138dc01be57a9b453c2af01
Димитър Янакиев <dyanakiev@codingburgas.bg>
2024-04-15 13:03:57 +0300

    Create tasks.cpp

---
+++ LibrarySystem/tasks.cpp
@@ -0,0 +1,9 @@
+// TODO: Set up a new C++ project for the library management system.
+// TODO: Lookup and create files, representing the data models of your system.
+// TODO: Implement a repository class that helps with read/write to the data model files.
+// TODO: Create classes for representing books, library members, and the library itself.
+// TODO: Create a logbook to track your progress and any issues encountered during development.
+// TODO: Start working on the documentation from the given template.
+// TODO: Complete the Introduction section of the documentation.
+// TODO: Write the Purpose and Scope section of the documentation.
+// TODO: Describe the System Overview (Трета точка) in the documentation.
